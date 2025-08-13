19eacc79e1e8e874f0917bd296ad11a7341425a5
dev-malvin <malvintech01@gmail.com>
2025-08-13 21:37:52 +0200

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "malvin~cuVEDbbQ#oNU1FVr4w1ryl28F3Az6vZPbLjTNjOmzmHSFpOc8ppQ",  // add your Session Id here
+  SESSION_ID: "",  // add your Session Id here
   
-  OWNER_NUMBER: "263776388689", // put your phone number here
+  OWNER_NUMBER: "263714757857", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
